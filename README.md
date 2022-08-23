34183edc2bcf45acafc27d290f5a041153a77d25
B4mbus <peaceofloneliness@gmail.com>
2022-08-23 13:49:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+
+# Nvim-headline - a simple and opinionated winbar
+
+</div>
+
+Nvim-headline is a simple winbar plugin with sane defaults, that requires barely any configuration (tho is still highly configurable).
+
+
+It's not meant to be a beast, it's meant to be simple and ad-hoc. If you want a powerful plugin extensible in all the possible ways use [lualine](https://github.com/nvim-lualine/lualine.nvim).
+
+TODO: TOC
+## Table Of Contents
+
+- [ ] Installation
+- [ ] Usage
+- [ ] Configuration
+- [ ] ???
---
+++ packspec.lua
@@ -0,0 +1,18 @@
+package = 'nvim-headline'
+version = '0.1.0'
+source = 'git://github.com/B4mbus/nvim-headline.git'
+description = {
+   summary = 'A simple winbar plugin',
+   detailed = [[
+    Nvim-headline is a simple and opinionated winbar plugin with sane defaults. It has sane defaults but is still highly configurable.
+   ]],
+   homepage = 'git://github.com/B4mbus/nvim-headline',
+   license = 'MIT'
+}
+
+dependencies = {
+   neovim = {
+      version = ">= 0.8.0",
+      source = "git://github.com/neovim/neovim.git"
+   },
+}
