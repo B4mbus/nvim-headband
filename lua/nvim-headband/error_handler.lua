c386911fc177617158ef74aa4ba66675dab2e21d
Daniel Zaradny <danielzaradny@gmail.com>
2022-08-26 02:27:39 +0200

    feat: new error notifying function

---
+++ lua/nvim-headband/error_handler.lua
@@ -0,0 +1,21 @@
+local ErrorHandler = {}
+
+--- Issues a notify_once notification with an error, deffered by @timeout milliseconds
+---@param content string The error message to display
+---@param timeout number | nil The timeout after which the notification should be shown, 100 by default
+ErrorHandler.headband_notify_error_deffered = function(content, timeout)
+  local actual_timeout = timeout or 100
+
+  vim.defer_fn(
+    function()
+      vim.notify(
+        content,
+        vim.log.levels.ERROR,
+        { title = 'nvim-headband' }
+      )
+    end,
+    actual_timeout
+  )
+end
+
+return ErrorHandler
