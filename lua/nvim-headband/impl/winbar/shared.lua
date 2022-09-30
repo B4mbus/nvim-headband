8590348ea4b41801158cb4b6adc0a71fd64d0db7
Daniel Zaradny <danielzaradny@gmail.com>
2022-09-30 22:43:03 +0200

    refactor: change the name of `evaluate_wrap` to `create_wrapper`

--- lua/nvim-headband/impl/winbar/shared.lua
+++ lua/nvim-headband/impl/winbar/shared.lua
@@ -14,7 +14,7 @@ local function wrap_function(wrap_pre, wrap_post)
   end
 end
 
-function SectionShared.evaluate_wrap(wrap)
+function SectionShared.create_wrapper(wrap)
   if not wrap then
     return empty_wrap_function
   end
@@ -27,13 +27,13 @@ function SectionShared.evaluate_wrap(wrap)
     wrap_pre, wrap_post = wrap[1], wrap[2]
   end
 
-  local Checkers = require 'nvim-headband.impl.checkers'
+  local Checkers = require('nvim-headband.impl.checkers')
   if wrap_pre and not Checkers.concatenable(wrap_pre) then
-    issue_wrap_concanetable_error 'pre'
+    issue_wrap_concanetable_error('pre')
   end
 
   if wrap_post and not Checkers.concatenable(wrap_post) then
-    issue_wrap_concanetable_error 'post'
+    issue_wrap_concanetable_error('post')
   end
 
   return wrap_function(wrap_pre, wrap_post)
