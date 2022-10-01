72eb67de0a5d4ba5485de9fe4010f4b001280b2a
Daniel Zaradny <danielzaradny@gmail.com>
2022-10-01 15:02:15 +0200

    feat: highlight icon's background according to text location's hls

--- lua/nvim-headband/impl/winbar/location_section.lua
+++ lua/nvim-headband/impl/winbar/location_section.lua
@@ -1,5 +1,6 @@
 local hl = require('nvim-headband.impl.utils').hl
 local empty_hl = require('nvim-headband.impl.utils').empty_hl
+local Highlights = require('nvim-headband.impl.highlights')
 
 local function issue_lack_of_location_provider_error()
   local ErrorHandler = require('nvim-headband.impl.error_handler')
@@ -54,9 +55,48 @@ local function get_location_icons(config)
   end
 end
 
-local function hl_icon(name, icon)
-  local highlight = hl('NavicIcons' .. name)
+local function get_text_highlight()
+  return Highlights.highlight_definition('NvimHeadbandLocText')
+end
 
+local function create_and_activate_hl_definition(original_name, patched_name, keep_foreground)
+  local original_hl_def = Highlights.highlight_definition(original_name)
+  local highlight_next_to_icon_bg = get_text_highlight().background
+
+  original_hl_def.background = highlight_next_to_icon_bg
+
+  if not keep_foreground then
+    original_hl_def = Highlights.remove_fg_from_definiton(original_hl_def)
+  end
+
+  vim.api.nvim_set_hl(0, patched_name, original_hl_def)
+end
+
+local function create_local_patched_hl(original_name, keep_foreground)
+  local patched_name = 'NvimHeadband' .. original_name .. 'Patched'
+
+  if not Highlights.hl_exists(patched_name) then
+    create_and_activate_hl_definition(original_name, patched_name, keep_foreground)
+  end
+
+  return patched_name
+end
+
+local function hl_name(name)
+  return hl('NvimHeadbandLocText') .. name .. empty_hl
+end
+
+local function highlight_empty_symbol(symbol)
+  return hl('NvimHeadbandEmptyLocSymbol') .. symbol .. empty_hl
+end
+
+local build_separator = function(sep)
+  return hl('NvimHeadbandLocSeparator') .. ' ' .. sep .. ' ' .. empty_hl
+end
+
+local LocationSection = {}
+
+function LocationSection:hl_icon(name, icon)
   --[[
     HACK:
     Hacky asf but nvim navic pads icons with spaces on the left and for some fucking reason
@@ -70,14 +110,19 @@ local function hl_icon(name, icon)
       and icon
       or icon:sub(1, -2)
 
-  return highlight .. formatted_icon .. empty_hl
+  local highlight_name = 'NavicIcons' .. name
+
+  if Highlights.definition_has_bg(get_text_highlight()) then
+    highlight_name = create_local_patched_hl(
+      highlight_name,
+      not (self.config.highlights.location_icons == 'none') -- if the highlights are not set to none, keep foreground
+    )
+  end
+
+  return hl(highlight_name) .. formatted_icon .. empty_hl
 end
 
-local function hl_name(name)
-  return hl('NvimHeadbandLocText') .. name .. empty_hl
-end
-
-local function get_raw_locations_items(data, reverse)
+function LocationSection:get_raw_locations_items(data, reverse)
   if not data or next(data) == nil then
     return nil
   end
@@ -88,24 +133,14 @@ local function get_raw_locations_items(data, reverse)
 
       -- TODO: Refactor maybe? Looks hella unreadable right now.
       if reverse then
-        return hl_name(item.name) .. (icon_empty and (' ' .. hl_icon(item.type, item.icon)) or '')
+        return hl_name(item.name) .. (icon_empty and (' ' .. self:hl_icon(item.type, item.icon)) or '')
       else
-        return (icon_empty and (hl_icon(item.type, item.icon) ..  ' ') or '') .. hl_name(item.name)
+        return (icon_empty and (self:hl_icon(item.type, item.icon) ..  ' ') or '') .. hl_name(item.name)
       end
     end,
     data
   )
 end
-
-local function highlight_empty_symbol(symbol)
-  return hl('NvimHeadbandEmptyLocSymbol') .. symbol .. empty_hl
-end
-
-local build_separator = function(sep)
-  return hl('NvimHeadbandLocSeparator') .. ' ' .. sep .. ' ' .. empty_hl
-end
-
-local LocationSection = {}
 
 function LocationSection:setup_location_provider(location_provider)
   location_provider.setup {
@@ -123,7 +158,7 @@ function LocationSection:get_location(mod)
     data = vim.fn.reverse(data)
   end
 
-  local raw_location_items = get_raw_locations_items(data, self.config.reversed)
+  local raw_location_items = self:get_raw_locations_items(data, self.config.reversed)
 
   if not raw_location_items then
     local symbol = self.config.empty_symbol
