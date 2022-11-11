96aec67b3e0b6ea9197efc699fe4e69ae924a4bd
Daniel Zaradny <danielzaradny@gmail.com>
2022-11-11 19:53:00 +0100

    refactor: don't redefine a new local variable

--- lua/nvim-headband/impl/error_handler.lua
+++ lua/nvim-headband/impl/error_handler.lua
@@ -4,7 +4,7 @@ local ErrorHandler = {}
 ---@param content string The error message to display
 ---@param timeout number? The timeout after which the notification should be shown, 100 by default
 function ErrorHandler.headband_notify_error_deffered(content, timeout)
-  local timeout = timeout or 100
+  timeout = timeout or 100
 
   vim.defer_fn(
     function()
